534b3b11688b8c6a6724e7c06b010259997059cd
SemHoogteijling <73480193+SemHoogteijling@users.noreply.github.com>
2026-06-06 17:24:09 +0200

    Add files via upload

---
+++ TdF2026.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 30 16:38:20 2023
+
+@author: shoogtei
+
+
+"""
+
+import pandas as pd
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+import time
+
+st.set_page_config(layout="wide")
+
+if 'df_store_temp' not in st.session_state:
+    st.session_state['df_store_temp'] = pd.DataFrame()
+    
+
+plt.rcParams['svg.fonttype'] = 'none'
+
+SHEET_ID = "17zM7Xsnej3p8JjcKzxmsmH-hdsbsWx4HsmzzN8BXNVM"
+url = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv&gid=0"
+
+dfTeams = pd.read_csv(url)
+
+#%%
+st.write ("""
+          # KNF TOUR 2026
+          """
+          )
+
+
+teams = dfTeams.columns()
+
+
+radioTeam = st.selectbox('Select team:', teams)
+dfTeam = dfTeams[[radioTeam]]
+
+st.table(dfTeam)
