02b95a0952942646d83788fe6c60a738dd20f812
SemHoogteijling <73480193+SemHoogteijling@users.noreply.github.com>
2026-06-09 15:18:04 +0200

    Update TdF2026.py

--- TdF2026.py
+++ TdF2026.py
@@ -64,7 +64,7 @@ with tab2:
 
     if etap is not None:
         etap_int = int(etap)
-        st.write('Etappe ' + etap + ':' + dfTeksten[dfTeksten['Etappe_nr']==etap_int].iloc[0])
+        st.write('Etappe ' + etap + ':' + dfTekst[dfTekst['Etappe_nr']==etap_int].iloc[0])
 
 with tab3:
     st.write ("""
